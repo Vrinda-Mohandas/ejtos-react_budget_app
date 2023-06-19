4458ab71f69d6ab8fad4e6e8ccdd2be214181214
Vrinda-Mohandas <vrindacm91@gmail.com>
2023-06-19 01:01:56 -0400

    Added Expense Decrease function

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,31 +1,43 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
+import { FaPlusCircle, FaMinusCircle } from 'react-icons/fa';
 import { AppContext } from '../context/AppContext';
 const ExpenseItem = (props) => {
-    const { dispatch,currency } = useContext(AppContext);
-    const handleDeleteExpense = () => {
-        dispatch({
-            type: 'DELETE_EXPENSE',
-            payload: props.id,
-        });
+  const { dispatch, currency } = useContext(AppContext);
+  const handleDeleteExpense = () => {
+    dispatch({
+      type: 'DELETE_EXPENSE',
+      payload: props.id,
+    });
+  };
+  const increaseAllocation = (name) => {
+    const expense = {
+      name: name,
+      cost: 10,
     };
-    const increaseAllocation = (name) => {
-        const expense = {
-            name: name,
-            cost: 10,
-        };
-        dispatch({
-            type: 'ADD_EXPENSE',
-            payload: expense
-        });
+    dispatch({
+      type: 'ADD_EXPENSE',
+      payload: expense
+    });
+  }
+  const decreaseAllocation = (name) => {
+    const expense = {
+      name: name,
+      cost: 10,
     }
-    return (
-        <tr>
-        <td>{props.name}</td>
-        <td>{currency}{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
-        </tr>
-    );
+    dispatch({
+      type: 'RED_EXPENSE',
+      payload: expense
+    });
+  }
+  return (
+    <tr>
+      <td>{props.name}</td>
+      <td>{currency}{props.cost}</td>
+      <td><FaPlusCircle style={{ color: 'green' }} size='1.5em' onClick={event => increaseAllocation(props.name)}></FaPlusCircle></td>
+      <td><FaMinusCircle style={{ color: 'red' }} size='1.5em' onClick={event => decreaseAllocation(props.name)}></FaMinusCircle></td>
+      <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+    </tr>
+  );
 };
 export default ExpenseItem;
